ea0e8055d16d3f5f41e1e84ab15e34e1aa3ad9ff
Gembelton <gembetlonwork@gmail.com>
2021-03-24 23:58:51 +0300

    initial

---
+++ entities/location.py
@@ -0,0 +1,6 @@
+class Location():
+    def __init__(self,name,path):
+        self.name = name
+        self.image = path
+        self.in_game = True
+
---
+++ entities/player.py
@@ -0,0 +1,5 @@
+class Player():
+    def __init__(self,number):
+        self.number = number #Порядковый номер игрока
+        self.current_role = None
+
