f36e21a96979b358bedcbb3841e4e21235703a00
Gembelton <gembetlonwork@gmail.com>
2021-03-26 16:34:27 +0300

    deploying

--- app.py
+++ app.py
@@ -426,23 +426,23 @@ if "HEROKU" in list(os.environ.keys()):
     logger = telebot.logger
     telebot.logger.setLevel(logging.INFO)
 
-    server = Flask(__name__)
+    app = Flask(__name__)
 
 
-    @server.route("/bot", methods=['POST'])
+    @app.route("/bot", methods=['POST'])
     def getMessage():
         bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
         return "!", 200
 
 
-    @server.route("/")
+    @app.route("/")
     def webhook():
         bot.remove_webhook()
         bot.set_webhook(url="https://test-new-new.herokuapp.com")
         return "?", 200
 
 
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 60))
+    app.run(host="0.0.0.0", port=os.environ.get('PORT', 60))
 else:
     bot.remove_webhook()
     while True:
